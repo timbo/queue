39cdd03ea5c33268157a84d237709f20e39e96ec
timbo <tim.whittley@gmail.com>
2013-10-23 21:47:09 -0400

    fixture data displaying

--- js/app.js
+++ js/app.js
@@ -1,11 +1,59 @@
 Queue = Ember.Application.create();
 
+//Routing
+
 Queue.Router.map(function() {
-  // put your routes here
+  this.resource('guests', {path: '/'});
 });
 
-Queue.IndexRoute = Ember.Route.extend({
+Queue.GuestsRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.find('guest');
   }
 });
+
+//Controllers
+
+Queue.GuestsController = Ember.ArrayController.extend();
+
+
+// Models
+
+Queue.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+Queue.Guest = DS.Model.extend({
+  name: DS.attr('string')
+});
+
+// Fixtures
+
+Queue.Guest.FIXTURES = [
+  {
+    id: 1,
+    name: 'King Buzzo'
+  },
+  {
+    id: 2,
+    name: 'Patti Smith'
+  },
+  {
+    id: 3,
+    name: 'Lou Reed'
+  },
+  {
+    id: 4,
+    name: 'Black Francis'
+  },
+  {
+    id: 5,
+    name: 'Johnny Ramone'
+  },
+  {
+    id: 6,
+    name: 'Neil Young'
+  },
+  {
+    id: 7,
+    name: 'Joan Jett'
+  }
+];
